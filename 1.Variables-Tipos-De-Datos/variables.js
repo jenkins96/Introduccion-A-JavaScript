79f2195126c84b5d470bf9f463a5cfcfb3b89ac3
Adrian Jenkins <adrianjnkns@gmail.com>
2021-08-30 20:35:56 -0600

    Created first directory, added a file and some text

---
+++ 1.Variables-Tipos-De-Datos/variables.js
@@ -0,0 +1,25 @@
+/* 
+Que es una variable?
+Una variable es un espacio en memoria para almacenar un tipo de dato
+y asignar un valor.
+*/
+// Existen dos tipos de variables en JS:
+    // let ==> se utiliza para variables que sabemos van a modificarse en algun momento.
+    // const ===> se utiliza para variables constantes, que sabemos no van a cambiar
+// Ejemplo:
+let edad = 25;
+const nombre = "Adrian";
+/*
+ Mientras que edad puede cambiar de valor porque fue declarada con "let",
+ "nombre" no es capaz de cambiar valor porque fue declarada con "const"
+ */
+
+ // Existen dos formas de asignar valor a una variable:
+ /*
+    1) Asignacion por valor:
+
+    2) Asignacion por referencia:
+ */
+
+// Diferencias entre "var", "let", "const"
+// Hoisting:
