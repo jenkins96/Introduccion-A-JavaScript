a70db43e7a41ea56d140db3aa0f7ceccee5227a6
Adrian Jenkins <adrianjnkns@gmail.com>
2021-09-08 15:53:00 -0600

    Created file

---
+++ 1.Variables-Tipos-De-Datos/tiposDeDatos.js
@@ -0,0 +1,16 @@
+
+/*======================================================
+ * TIPOS DE DATOS
+ =====================================================*/
+ // Existen dos formas de asignar valor a una variabl o constante:
+    // 1) Ejemplo Asignacion por valor:
+    let edadPedro = 12;
+    let edadMario = edadPedro;
+    edadMario++
+    console.log(edadPedro);
+    console.log(edadMario);
+    // Se le asigna a 'edadPedro' un valor de 12
+    // Se le asigna a 'edadMario"
+ //   2) Asignacion por referencia:
+    
+
