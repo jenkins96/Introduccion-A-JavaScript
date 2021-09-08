16504f44a43b543f303ce7dec2cc3f82106a49db
Adrian Jenkins <adrianjnkns@gmail.com>
2021-09-08 15:52:15 -0600

    Added content

--- 1.Variables-Tipos-De-Datos/variables.js
+++ 1.Variables-Tipos-De-Datos/variables.js
@@ -2,6 +2,22 @@
                 VARIABLES Y CONSTANTES
  =========================================================*/
 /* 
+En su nivel mas bajo, el lenguaje de las computadoras es la electricidad.
+Para indicar una instruccion o cualquier otra cosas, esta se transmite a traves
+de pulsos o no pulsos de electricidad.Es decir, se abre el circuito para dejar
+pasar el pulso de energia o se cierra para que no pase.
+En las computadoras la informacion es guardada como una secuencia de 'bits'.
+Un 'bit' es la unidad minima de informacion y se representa como cualquier
+cosa que tenga dos posibles valores. Para este caso se describen como '1'
+y '0'.Donde el primero significa puslo de electricidad y el ultimo es una
+ausencia de puslo.
+Cualquier informacion de tipo discreto puede ser reducida a una secuencia de
+unos y ceros, por tanto, representables en bits.
+En una computadora existe una cantidad finita de memoria. Cuando nosotros 
+guardamos informacion esta se representara en forma de 'bits' de acuerdo
+al tipo de informacion y su valor y ocupara un espacio en la memoria. Cuando
+esa informacion no sea necesaria se eliminara, brindando ese espacio para nueva
+informacion.
 QUE ES UNA VARIABLE?
 *Una variable es un espacio en memoria para almacenar un tipo de dato
 y asignar un valor.Como su nombre lo indica, su valor es variable, esto
@@ -37,16 +53,15 @@ console.log(NOMBRE_APELLIDO);
   */
 // Puesto es una constante, no es posible modificar su valor:
     NOMBRE_APELLIDO = 'Pedrito Micasa'; // Error
-// --------------
- // Existen dos formas de asignar valor a una variabl o constante:
- /*
-    1) Asignacion por valor:
-
-    2) Asignacion por referencia:
- */
+    
 
 // Diferencias entre "var", "let", "const"
-// Hoisting:
+/*
+ Hoisting: Proceso mediante el cual, en la fase de compilacion del motor
+ de JS las declaraciones de variables y funciones son asignadas en memoria,
+ por tanto, pueden ser accesadas antes de que tecnicamente sean creadas.
+
+*/
 // Formas de declarar variables:
 let var1, var2, var3;
 let var4, var5 = 5, var6 = 5;
